7e8ec061bd34dd98161963eacc7d086c047d5db0
Jakub Roszczenko <typekzkantorka@gmail.com>
2026-08-11 18:04:22 +0200

    fix format pep8

--- main.py
+++ main.py
@@ -1,40 +1,42 @@
 from functions import Subject, Task
 
 MENU = """
-========================================
-         📚 STUDYFLOW - MENU 📚
-========================================
- [dodajprzedmiot] - Dodaj nowy przedmiot
- [dodajzadanie]   - Dodaj nowe zadanie
- [pokaz]          - Wyświetl wszystkie zadania
- [wykonano]       - Oznacz zadanie jako zrobione
- [wyjdz]          - Zamknij program
-========================================
+📚 STUDYFLOW - MENU 📚
+
+Wybierz jedną z opcji:
+- dodajprzedmiot
+- pokaz
+- dodajzadanie
+- wykonano
+- wyjdz
 """
 
 
 def main():
-
     subject = Subject()
     task = Task()
 
     while True:
-        wybor = input("\nWybierz opcję > ").lower().strip().replace(" ", "")
+        wybor = (
+            input("\nWybierz opcję > ")
+            .lower()
+            .strip()
+            .replace(" ", "")
+        )
 
-        if wybor == 'dodajprzedmiot':
-           subject.add()
-        elif wybor == 'pokaz':
+        if wybor == "dodajprzedmiot":
+            subject.add()
+        elif wybor == "pokaz":
             task.show()
-        elif wybor == 'dodajzadanie':
+        elif wybor == "dodajzadanie":
             task.add()
-        elif wybor == 'wykonano':
+        elif wybor == "wykonano":
             task.complete()
-        elif wybor == 'wyjdz':
+        elif wybor == "wyjdz":
             print("\nMiłej nauki! Do zobaczenia!")
             break
         else:
-            print(" Niepoprawna komenda! Spróbuj ponownie.")
-            continue
+            print("Niepoprawna komenda! Spróbuj ponownie.")
 
 
 if __name__ == "__main__":
